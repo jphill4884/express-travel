4008deeac446c29d2e976621178fcb22a3f498e8
Kyle Phillips <47147991+jphill4884@users.noreply.github.com>
2022-06-18 14:06:39 +0200

    Update countries.js

--- countries.js
+++ countries.js
@@ -1,1 +1,35 @@
+const countries = [
+  {
+    id: 1,
+    name: "Egypt",
+    alpha2Code: "EG",
+    alpha3Code: "EGT",
+  },
+  {
+    id: 2,
+    name: "Botswana",
+    alpha2Code: "BW",
+    alpha3Code: "BWA",
+  },
+  {
+    id: 3,
+    name: "Namibia",
+    alpha2Code: "NA",
+    alpha3Code: "NAM",
+  },
+  {
+    id: 4,
+    name: "South Africa",
+    alpha2Code: "ZA",
+    alpha3Code: "ZAF",
+  },
+  {
+    id: 5,
+    name: "Chile",
+    alpha2Code: "CL",
+    alpha3Code: "CHL",
+  },
+];
+
+module.exports = countries;
 
