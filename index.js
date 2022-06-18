0a55295ec6d8f58dd9f4a45cbcffd06e84559ad7
Kyle Phillips <47147991+jphill4884@users.noreply.github.com>
2022-06-18 14:06:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const wishlistRouter = require("./wishlistRouter.js");
+const countries = require("./countries.js");
+
+const app = express();
+app.use(express.json());
+app.use("/api/countries", wishlistRouter);
+
+app.get("/api/countries", (req, res, next) => {
+  //console.log(req);
+  res.send(countries);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App is listening to Port: ${port}`);
+});
 
