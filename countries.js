6d6961e71ebeedc95704b8395279385c0439603d
Kyle Phillips <justinkphillips@gmail.com>
2022-06-18 14:16:05 +0200

    sorting commit issues

--- countries.js
+++ countries.js
@@ -1,39 +1,5 @@
 const countries = [
-<<<<<<< HEAD
-    {
-	"id" : 1,
-	"name" : "Egypt",
-	"alpha2Code": "EG",
-	"alpha3Code": "EGT",
-},
-{
-	"id" : 2,
-	"name" : "Botswana",
-	"alpha2Code": "BW",
-	"alpha3Code": "BWA",
-},
-{
-	"id" : 3,
-	"name" : "Namibia",
-	"alpha2Code": "NA",
-	"alpha3Code": "NAM",
-},
-{
-	"id" : 4,
-	"name" : "South Africa",
-	"alpha2Code": "ZA",
-	"alpha3Code": "ZAF",
-},
-{
-	"id" : 5,
-	"name" : "Chile",
-	"alpha2Code": "CL",
-	"alpha3Code": "CHL",
-},
-];
 
-module.exports = countries;
-=======
   {
     id: 1,
     name: "Egypt",
@@ -67,5 +33,3 @@ module.exports = countries;
 ];
 
 module.exports = countries;
-
->>>>>>> 2712042dab4d9bfba04b65a0b922e8bb575182ee
